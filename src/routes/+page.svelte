299a40d2d33f854849dfc47bb802276c449db872
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-05-12 18:07:41 +0800

    way for player to add bet implemented

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
+    import * as Select from "$lib/components/ui/select/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+
     // cards
     interface Game {
         Players: Player[]
         Team1: number[] // winner of bet
         Team2: number[]
         Trump: string // trump suit
-        Bet: number
+        BetSize: number
+        IsBettingPhase: boolean
     }
 
     interface Card {
@@ -18,6 +24,11 @@
         ID: number // 1-4
         Cards: Card[]
         Sets: number
+    }
+
+    interface Move {
+        CardPlayed: Card
+        Player: number
     }
 
     const valueToRank = new Map([
@@ -99,25 +110,65 @@
             Team1: [0, 1],
             Team2: [2, 3],
             Trump: "Spades",
-            Bet: 1
+            BetSize: 1,
+            IsBettingPhase: true,
         }
     }
 
     let game = initGame()
 
-    function RaiseBet(betSize: number, suit: string) {
+    let betSize: number
+    let bettedSuit: string = "Club"
+
+    function raiseBet() {
+        game.BetSize = betSize
+        game.Trump = bettedSuit
+        game.IsBettingPhase = false
+    }
+
+    function playCard(card: Card) {
 
     }
 </script>
 
-<div class="flex gap-2">
-    {#each game.Players as player}
+<div class="flex flex-col gap-10 w-full h-screen justify-center items-center">
     <div>
-        <p>P{player.ID}:</p>
-        {#each player.Cards as card}
-        <div>{card.Rank} {card.Suit}</div>
+        <p>Trump Suit: {game.Trump}</p>
+        <p>Bet Size: {game.BetSize}</p>
+    </div>
+    <div class="flex gap-10">
+        {#each game.Players as player}
+        <div>
+            <p>P{player.ID}:</p>
+            {#each player.Cards as card}
+            <div>{card.Rank} {card.Suit}</div>
+            {/each}
+        </div>
         {/each}
     </div>
-    {/each}
+
+    {#if game.IsBettingPhase}
+    <div class="flex flex-col justify-center items-center gap-2">
+        <div class="flex gap-2">
+            <Input bind:value={betSize} type="number" placeholder="1 to 7"/>
+    
+            <Select.Root type="single" bind:value={bettedSuit}>
+            <Select.Trigger class="w-[180px]">
+                {bettedSuit}
+            </Select.Trigger>
+            <Select.Content>
+                <Select.Item value="Club">Club</Select.Item>
+                <Select.Item value="Diamond">Diamond</Select.Item>
+                <Select.Item value="Heart">Heart</Select.Item>
+                <Select.Item value="Spades">System</Select.Item>
+            </Select.Content>
+            </Select.Root>
+        </div>
+        <div class="flex gap-2">
+            <Button>Pass</Button>
+            <Button onclick={raiseBet}>Raise</Button>
+        </div>
+    </div>
+    {/if}
 </div>
 
