53a46871cc8a942ed17a9f90cba8092881614fbd
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-05-12 18:07:41 +0800

    plugged my github

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import { page } from '$app/state';
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit'
-	import { Banana, History } from '@lucide/svelte';
+	import { Banana, History, Github } from '@lucide/svelte';
 	
 	let { children } = $props();
 	injectAnalytics({ mode: dev ? 'development' : 'production' })
@@ -15,6 +15,7 @@
 	<div class="flex gap-3">
 		<a href="/" class={page.url.pathname === "/" ? "opacity-100" : "opacity-55"}><Banana /></a>
 		<a href="/history" class={page.url.pathname.startsWith("/history") ? "opacity-100" : "opacity-55"}><History /></a>
+		<a href="https://github.com/bedminer1/BRIDGE" class="opacity-55" target="_blank"><Github /></a>
 	</div>
 </div>
 {@render children()}
