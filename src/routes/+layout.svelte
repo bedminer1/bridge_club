6ff067a5cd7e87b348d1de2b5fce313c8b9df59e
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-05-12 18:07:41 +0800

    align top icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <ModeWatcher />
-<div class="absolute top-2 left-2">
+<div class="absolute top-4 left-4">
 	<div class="flex gap-3">
 		<a href="/" class={page.url.pathname === "/" ? "opacity-100" : "opacity-55"}><Banana /></a>
 		<a href="/history" class={page.url.pathname === "/history" ? "opacity-100" : "opacity-55"}><History /></a>
