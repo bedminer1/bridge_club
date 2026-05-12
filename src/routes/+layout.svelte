ed0ef94277f0969035f8bd3a69269e24ea708f6b
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-05-12 18:07:41 +0800

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from "mode-watcher";
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' })
 </script>
 
 <ModeWatcher />
