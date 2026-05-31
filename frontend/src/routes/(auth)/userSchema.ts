3ecae44548800bcb9d041b199e821aab35efcf44
Alexander Seah <alexander.seah.min.rui@gmail.com>
2026-06-01 03:17:13 +0800

    fix login page.ts path (wrong escaped dir)

--- frontend/src/routes/(auth)/userSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
-export const userFormSchema = z.object({
-    username: z.string().min(2).max(50),
-    password: z.string().min(2).max(50),
-})
-
-export type UserFormSchema = typeof userFormSchema
