db8b9c595c257ee94829aa38a1b20dd9b5acef0d
Alexander Seah <alexander.seah.min.rui@gmail.com>
2026-06-29 18:02:04 +0800

    removed playground from site-header

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ npm run build
 echo "=== Deploying to server ==="
 ssh root@178.105.168.55 "cd /root/bridge_club && \
   git pull origin main && \
-  cd frontend && npm run build && \
+  cd frontend && npm install && npm run build && \
   rm -rf /var/www/bridge-club && cp -r build /var/www/bridge-club && \
   cd /root/bridge_club/backend && source \"\$HOME/.cargo/env\" && cargo build --release -p bridge-server && \
   systemctl restart bridge-server"
